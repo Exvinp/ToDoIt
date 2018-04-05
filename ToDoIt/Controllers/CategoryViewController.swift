c23d2fe0804a09549a8f9f922879e0c02a5af744
NB <nb@2a01-036d-0103-104d-089e-57f1-263d-b583.pool6.digikabel.hu>
2018-04-05 12:09:37 +0200

    Category viewcontroller without delegation to items.

---
+++ ToDoIt/Controllers/CategoryViewController.swift
@@ -0,0 +1,105 @@
+//
+//  CategoryViewController.swift
+//  ToDoIt
+//
+//  Created by NB on 2018. 04. 05..
+//  Copyright © 2018. coolCodeAgency. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+
+//ez a ViewController ugyan arra az alapra épül mint a ToDoListViewController leszámítva azt hogy abban lehet keresni is, ezért kicsit át van alakítva
+class CategoryViewController: UITableViewController {
+    
+    var categories = [Category]()
+    
+    //ezzel a kóddal az AppDelegate classból csinálunk egy object-et, persistentContainer property-jének viewContext porperty-je
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadCategories()
+    }
+    
+
+    
+    
+//MARK: - Tableview Datasource Methods ////////////////////////////////////////////////////////////
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        
+        cell.textLabel?.text = categories[indexPath.row].name
+        
+        
+        return cell
+    }
+    
+   
+//MARK: - Data Manipulation Methods ///////////////////////////////////////////////////////////////
+    func saveCategories(){
+        
+        do {
+            try context.save()
+        }catch{
+            print(error)
+        }
+
+        tableView.reloadData()
+        
+    }
+    
+    
+    func loadCategories(){
+        let request: NSFetchRequest<Category> = Category.fetchRequest()
+        
+        do{
+            categories = try context.fetch(request)
+        }catch{
+            print(error)
+        }
+        tableView.reloadData()
+    }
+    
+    
+//MARK: - Add new Categories //////////////////////////////////////////////////////////////////////
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
+            self.categories.append(newCategory)
+            self.saveCategories()
+        }
+        
+        alert.addAction(action)
+        
+        alert.addTextField { (field) in
+            textField = field
+            field.placeholder = "Create new Category"
+        }
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+//MARK: - Tableview Delegate Methods //////////////////////////////////////////////////////////////
+    
+    
+    
+    
+
+    
+}
--- ToDoIt/Controllers/ToDoListViewController.swift
+++ ToDoIt/Controllers/ToDoListViewController.swift
@@ -7,18 +7,19 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoListViewController: UITableViewController {
 
     var itemArray = [Item]()
     
-    //ez egy array (ezért lehet .first) singleton object, itt hozzuk létre a Items.plist-et
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    //ezzel a kóddal az AppDelegate classból csinálunk egy object-et, persistentContainer property-jének viewContext porperty-je
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         loadItems()
         
     }
@@ -49,8 +50,13 @@ class ToDoListViewController: UITableViewController {
 //MARK: - Tableview Delegate Methods //////////////////////////////////////////////////////////////
     //akkor fut le amikor egy sort kiválasztunk
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("\(itemArray[indexPath.row])")
-        print("\(itemArray[indexPath.row].title)")
+        
+        //itemArray[indexPath.row].setValue("Completed", forKey: "title")
+        
+        //törlés, az első a DB-ből törli, a második a tableview-ból, fontos a sorrend!!!!
+        //context.delete(itemArray[indexPath.row])
+        //itemArray.remove(at: indexPath.row)
+        
         
         //ha a kiválasztott sor done-ja false (nincs pipa) akkor rakja ki a pipát, és fordítva
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
@@ -64,7 +70,7 @@ class ToDoListViewController: UITableViewController {
 
     
     
-//MARK: - Add New Items ///////////////////////////////////////////////////////////////////////////////
+//MARK: - Add New Items //////////////////////////////////////////////////////////////////////////////////
     //akkor fut le amikor a hozzáadás gomb (plusz jel) pressed
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -75,14 +81,12 @@ class ToDoListViewController: UITableViewController {
         
         //itt adjuk hozzá a gombot
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            
             //ez fog történni amikor a user ráklikkel az "Add Item" gombra
-            let newItem = Item()
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
-            
+            newItem.done = false
             self.itemArray.append(newItem)
             self.saveItems()
-
         }
         
         //ez egy closure, az alertTextField egy ideiglenes változó benne amit mi hozunk létre
@@ -90,6 +94,7 @@ class ToDoListViewController: UITableViewController {
             alertTextField.placeholder = "Create new Item"
             textField = alertTextField
         }
+        
         //itt adjuk hozzá az alert felugró ablakhoz az action gombot
         alert.addAction(action)
         
@@ -104,31 +109,77 @@ class ToDoListViewController: UITableViewController {
 //MARK: - Model Manipulation Methods ///////////////////////////////////////////////////////////////
     
     func saveItems(){
-        
-        let encoder = PropertyListEncoder()
-        
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         }catch{
-            print("Error encoding item array \(error)")
+            print(error)
         }
         
         self.tableView.reloadData()
-        
-        
+
     }
     
     
+    //with külső paraméter, request a belső paraméter
+    //ha a request paraméter nincs megadva a híváskor akkor az alapértelmezett értéke az Item DB fetchRequest() metódusa
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()){
+        
+        //a request object típusa NSFetchRequest<Item>, az Item DB fetchRequest()-jét hívja meg
+        //let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        do{
+            itemArray = try context.fetch(request)
+        }catch{
+            print(error)
+        }
+    }
+
+
+}
+
+
+
+//MARK: - Search bar methods ////////////////////////////////////////////////////////////////////////////
+extension ToDoListViewController: UISearchBarDelegate{
     
-    func loadItems(){
-        if let data = try? Data(contentsOf: dataFilePath!){
-            let decoder = PropertyListDecoder()
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        //a request object típusa NSFetchRequest<Item>, az Item DB fetchRequest()-jét hívja meg
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
+
+        //lekérdezés object, NSPredicate fv, title-ben keressük, CONTAINS = tartalmazza, [cd] nem case(Aa) és diacritic(aá) sensitive, %@ amit keresünk
+        //searchBar.text ahonnan jön amit keresünk
+        //let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        //
+        //maga a lekérdezés request predicate metódusa a predicate object-tel történjen
+        //request.predicate = predicate
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        
+        //sorting (rendezés), titile szerint, abc sorrendben
+        //let sortDescriptr = NSSortDescriptor(key: "title", ascending: true)
+        //
+        //egy array-t vár el a sortDescriptors, ezért egy single array-t kap amiben a sortDescriptr van, de ezt a kettő utasítást egyban is meg lehet adni
+        //request.sortDescriptors = [sortDescriptr]
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+
+    }
+    
+    
+    //akkor amikor a searchBar-ban lévő elemek száma 0-ra változik (azaz nem betöltéskor) akkor vissza viszi a usert
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0{
+            loadItems()
             
-            do{
-                itemArray = try decoder.decode([Item].self, from: data)
-            } catch{
-                print(error)
+            //a DispatchQueue az aktív eszközök kikapcsolását szabályozó rendszer, különböző thread-ekbe tudja sorolni a folyamatot
+            //mia main thread-be akarjuk kinyirnia billentyűt (hogy gyors legyen)
+            DispatchQueue.main.async {
+                //megszünik fő beszélőnek lenni
+                searchBar.resignFirstResponder()
             }
         }
     }
@@ -136,17 +187,18 @@ class ToDoListViewController: UITableViewController {
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
